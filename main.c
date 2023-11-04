fe46bb6151073c9f600269b03a7a2949792a3e4a
Emad Obeidat <emadobeidat217@gmail.com>
2023-11-04 08:43:45 +0300

    files

--- main.c
+++ main.c
@@ -9,12 +9,12 @@ void printSudoku(int sudoku[G][G]) {
         printf("|");
         for (int j= 0; j <G; j++) {
             printf("%2d", sudoku[i][j]);
-            if ((j + 1)% 3== 0 &&j <G -1) {
+            if ((j+1)%3== 0&&j <G-1) {
                 printf("|"); // Separate columns within subgrids
             }
         }
         printf("|\n");
-        if ((i + 1)% 3== 0&& i <G -1) {
+        if ((i+1)% 3== 0&&i <G-1) {
             printf("+------+------+------+\n"); // Separate rows between subgrids
         }
     }
@@ -22,8 +22,8 @@ void printSudoku(int sudoku[G][G]) {
 
 
 void shuffle(int array[G]) {
-    for (int i= G -1; i >0; i--) {
-        int j= rand()% (i + 1);
+    for (int i= G-1; i >0; i--) {
+        int j= rand()% (i+1);
         int temp= array[i];
         array[i]= array[j];
         array[j]= temp;
@@ -31,7 +31,7 @@ void shuffle(int array[G]) {
 }
 
 void generateSudoku(int sudoku[G][G]) {
-    int base[G]= {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int base[G]= {1,2,3,4,5,6,7,8,9};
 
     //random number generator
     srand(time(NULL));
@@ -58,7 +58,7 @@ int main() {
     generateSudoku(sudoku);
     clock_t endTime= clock();
 
-    double totalTime= (double)(endTime - startTime)/ CLOCKS_PER_SEC;
+    double totalTime= (double)(endTime-startTime)/ CLOCKS_PER_SEC;
     printSudoku(sudoku);
 
     printf("Time taken: %f seconds\n",totalTime);
